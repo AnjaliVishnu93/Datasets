b3e6b28a8be500809f820dfafd4f1491dac29425
AnjaliVishnu93 <138982592+AnjaliVishnu93@users.noreply.github.com>
2023-09-16 00:49:38 +0530

    Add files via upload

---
+++ PrologRulesDec.pl
@@ -0,0 +1,15 @@
+% Rules derived from the decision tree
+direction(c0) :- a3(false), a10(false).
+direction(c3) :- a3(false), a10(true), a2(false), a13(false).
+direction(c0) :- a3(false), a10(true), a2(false), a13(true), a0(false).
+direction(c3) :- a3(false), a10(true), a2(false), a13(true), a0(true).
+direction(c0) :- a3(false), a10(true), a2(true).
+direction(c1) :- a3(true), a0(false), a8(false).
+direction(c0) :- a3(true), a0(false), a8(true), a10(false).
+direction(c1) :- a3(true), a0(false), a8(true), a10(true).
+direction(c2) :- a3(true), a0(true), a10(false), a8(false), a4(false).
+direction(c3) :- a3(true), a0(true), a10(false), a8(false), a4(true).
+direction(c0) :- a3(true), a0(true), a10(false), a8(true).
+direction(c1) :- a3(true), a0(true), a10(true), a5(false).
+direction(c0) :- a3(true), a0(true), a10(true), a5(true), a6(false).
+direction(c3) :- a3(true), a0(true), a10(true), a5(true), a6(true).
